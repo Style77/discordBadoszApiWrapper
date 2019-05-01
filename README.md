43b6a045ce485fb876a691f14b5ccbb8d5d7e1ee
Style77 <Paluczak2015@wp.pl>
2019-05-01 13:53:05 +0200

    update

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ Simple wrapper for simple(to use ofc) api :D
 To get token: `api.badosz.com/req`
 Example is in `example.py`
 Every function which return `Buffer` is converted to `discord.File`. If you want to know how to send it check examples.
-
-To install type in console following command: `pip install git+git://github.com/Style77/discordBadoszApiWrapper.git`
--- wrapper/utils.py
+++ wrapper/utils.py
@@ -10,15 +10,12 @@ class utils():
         self.bot = bot
 
     def processing(self, image) -> BytesIO:
-        with Image.open(StringIO(image.content)) as im:
-            final_buffer = BytesIO()
-            im.save(final_buffer, "png")
-
-        final_buffer.seek(0)
+        im = Image.open(image.content)
+        final_buffer = BytesIO()
+        im.save(final_buffer, "png")
         return final_buffer
 
     async def get_image(self, image):
-        #im = await utils.get_img_bytes(self, image)
         fn = partial(utils.processing, self, image)
         final_buffer = await self.bot.loop.run_in_executor(None, fn)
         return discord.File(fp=final_buffer)
