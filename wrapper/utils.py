8b8ba0925be3ff6602a201574b7277bd5f75d07f
Style77 <Paluczak2015@wp.pl>
2019-05-01 02:06:44 +0200

    Update utils.py
    okey this is last

--- wrapper/utils.py
+++ wrapper/utils.py
@@ -9,14 +9,8 @@ class utils():
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_img_bytes(self, image) -> bytes:
-        async with self.session.get(image) as response:
-            img_bytes = await response.read()
-
-        return img_bytes
-
-    def processing(self, image: bytes) -> BytesIO:
-        with Image.open(BytesIO(image)) as im:
+    def processing(self, image) -> BytesIO:
+        with Image.open(StringIO(image.content)) as im:
             final_buffer = BytesIO()
             im.save(final_buffer, "png")
 
@@ -25,6 +19,6 @@ class utils():
 
     async def get_image(self, image):
         #im = await utils.get_img_bytes(self, image)
-        fn = partial(utils.processing, self, image.read())
+        fn = partial(utils.processing, self, image)
         final_buffer = await self.bot.loop.run_in_executor(None, fn)
         return discord.File(fp=final_buffer)
