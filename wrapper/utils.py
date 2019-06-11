58c38dff41b1d96ba6960c3d60e6a40f3f5649ba
Style77 <Paluczak2015@wp.pl>
2019-06-11 20:46:15 +0200

    Update utils.py

--- wrapper/utils.py
+++ wrapper/utils.py
@@ -10,18 +10,6 @@ class utils():
     def __init__(self, bot):
         self.bot = bot
 
-    def processing(self, image, type_of_img) -> BytesIO:
-        im = Image.open(BytesIO(image.content))
-        if type_of_img in ["gif", "webm", "mp4"]:
-            final_buffer = BytesIO()
-            final_buffer.write(image.content)
-            final_buffer.seek(0)
-            return final_buffer
-        final_buffer = BytesIO()
-        im.save(final_buffer, type_of_img)
-        final_buffer.seek(0)
-        return final_buffer
-
     async def get_image(self, image):
         type_ = image.headers['Content-Type']
         formats_map = {
@@ -32,5 +20,8 @@ class utils():
             "video/mp4": "mp4",
             "video/webm": "webm"
         }
-        final_buffer = await self.bot.loop.run_in_executor(None, utils.processing, self, image, formats_map[type_])
-        return discord.File(fp=final_buffer, filename=f"nothing.{formats_map[type_]}")
+        #if formats_map[type_] in ["gif", "webl", "webm"]:
+            #im = image
+        #else:
+        image = BytesIO(await image.read())
+        return discord.File(fp=image, filename=f"nothing.{formats_map[type_]}")
